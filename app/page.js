5e344a9bd9bfd1c0ebc3295fa83f5070ec05e83a
kalyan <kalyanpanchal1415@gmail.com>
2025-03-30 14:55:28 +0530

    initial setup

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
    <main className="flex flex-col justify-center items-center p-8">
-    <h1 className="underline"> Detective Ai </h1>
+    <h1 className=""> Detective Ai </h1>
    </main>
   );
 }
